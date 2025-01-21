588f56f15764ad75535e9c468943bd2bb88e3c7d
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-01-21 11:02:13 +0100

    Add solution for Palindrome.

---
+++ TwoSum.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+
+        int[] output = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int numToFind = target - nums[i];
+
+            for (int g = i + 1; g < nums.Length; g++)
+            {
+                if (nums[g] == numToFind)
+                {
+                    output[0] = i;
+                    output[1] = g;
+                    return output;
+                }
+            }
+        }
+
+        return [];
+    }
+}
